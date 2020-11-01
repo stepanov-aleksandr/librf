c5de5a7536d8224bd943c6a138f680938b20ea4d
Stam <stamhoffman@yandex.ru>
2020-11-01 16:41:54 +0300

    [librf] fix Write

--- config.h
+++ config.h
@@ -6,9 +6,10 @@
 constexpr int MAX_SIZE_PACKED = 3;  ///< максимальный размер пакета
 
 const struct {
-  const std::string kIdMesseng = "[idm]";
-  const std::string kNumPacked = "[np]";
-  const std::string kSizePacked = "[sp]";
+  const std::string kIdMesseng = "idm";
+  const std::string kNumPacked = "np";
+  const std::string kSizePacked = "sp";
+  const std::string kData = "data";
 } TypeField;
 
 #endif  // CONFIG_H
